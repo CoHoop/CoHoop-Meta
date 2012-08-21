5d0365ee81087aff07f49d3752cff95774ed3ce2
gabriel-dehan <dehan.gabriel@gmail.com>
2012-08-21 23:40:34 +0200

    Database name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "CoHoop-Meta"
 set :deploy_to,   "/home/ubuntu/www/#{application}"
 
-set :location, '54.247.108.99'
+set :location, '54.247.139.88'
 
 role :web, location                          # Your HTTP server, Apache/etc
 role :app, location
@@ -14,13 +14,15 @@ set :scm, :git
 set :branch, "master"
 
 set :user, "ubuntu"
-set :use_sudo, true
+set :use_sudo, false
 set :admin_runner, "ubuntu"
 
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = ["#{ ENV['HOME']}/.ssh/KEY-CoHoop.pem"]
 
 set :rails_env, 'production'
+
+#set :rails_env, 'development'
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
@@ -28,13 +30,19 @@ set :rails_env, 'production'
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
+#namespace :deploy do
+#  task :start do ; end
+#  task :stop do ; end
+#  task :restart, :roles => :app, :except => { :no_release => true } do
+#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#  end
+#end
 
-#after 'deploy:setup', 'db:setup'
+#before 'deploy:setup', 'rvm:install_rvm'
+#set :rvm_install_type :stable
+#set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+set :rvm_ruby_string, "ruby-1.9.3-p194"
+require "rvm/capistrano"
+set :rvm_type, :user
+
 after :deploy, "deploy:migrate"
