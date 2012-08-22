9038e50eef1d7704ccdaa08699edd1367e1623ae
gabriel-dehan <dehan.gabriel@gmail.com>
2012-08-22 23:53:59 +0200

    Deployment & DB settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
+require 'bundler/capistrano'
+require "rvm/capistrano"
+
 set :application, "CoHoop-Meta"
 set :deploy_to,   "/home/ubuntu/www/#{application}"
 
-set :location, '54.247.139.88'
+set :location, '54.247.108.99'
 
 role :web, location                          # Your HTTP server, Apache/etc
 role :app, location
 role :db,  location, :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-default_run_options[:pty] = true
 set :repository, "git://github.com/CoHoop/CoHoop-Meta.git"
 set :scm, :git
 set :branch, "master"
@@ -17,6 +19,7 @@ set :user, "ubuntu"
 set :use_sudo, false
 set :admin_runner, "ubuntu"
 
+default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = ["#{ ENV['HOME']}/.ssh/KEY-CoHoop.pem"]
 
@@ -30,19 +33,19 @@ set :rails_env, 'production'
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-#namespace :deploy do
-#  task :start do ; end
-#  task :stop do ; end
-#  task :restart, :roles => :app, :except => { :no_release => true } do
-#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#  end
-#end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
 #before 'deploy:setup', 'rvm:install_rvm'
 #set :rvm_install_type :stable
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
-set :rvm_ruby_string, "ruby-1.9.3-p194"
-require "rvm/capistrano"
+
+set :rvm_ruby_string, "1.9.3"
 set :rvm_type, :user
 
 after :deploy, "deploy:migrate"
